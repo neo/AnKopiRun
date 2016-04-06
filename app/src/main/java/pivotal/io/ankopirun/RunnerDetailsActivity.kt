99c42cbc8dcac1f7a65058e2854d3f0853ee933d
Benjamin Tan + Natalie Tay <pair+bt+nt@pivotal.io>
2016-04-06 10:44:12 +0800

    Switch to Kotlin [#116859433]

---
+++ app/src/main/java/pivotal/io/ankopirun/RunnerDetailsActivity.kt
@@ -0,0 +1,11 @@
+package pivotal.io.ankopirun
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RunnerDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_runner_details)
+    }
+}
---
+++ app/src/main/java/pivotal/io/ankopirun/RunnerLocationActivity.kt
@@ -0,0 +1,11 @@
+package pivotal.io.ankopirun
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RunnerLocationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_runner_location)
+    }
+}
